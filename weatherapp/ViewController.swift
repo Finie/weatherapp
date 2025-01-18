6add288162469ff1503dde5b92688ad4b0809354
finie <finiephenius@gmail.com>
2025-01-18 05:22:31 +0300

    Initial Commit

---
+++ weatherapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherapp
+//
+//  Created by fin on 18/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
